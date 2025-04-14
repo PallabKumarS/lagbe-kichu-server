f7d137fe95fdf65303f08b1fce6a268acd0a25d2
PallabKumarS <pallabkumar26@gmail.com>
2025-04-15 01:35:31 +0600

    feat: enhance review creation and update listing rating

--- src/app/modules/review/review.service.ts
+++ src/app/modules/review/review.service.ts
@@ -1,10 +1,12 @@
-import { Types } from 'mongoose';
+import mongoose, { Types } from 'mongoose';
 import { TReview } from './review.interface';
 import ReviewModel from './review.model';
 import { AppError } from '../../errors/AppError';
 import httpStatus from 'http-status';
 import OrderModel from '../order/order.model';
 import QueryBuilder from '../../builder/QueryBuilder';
+import { TListing } from '../listing/listing.interface';
+import ListingModel from '../listing/listing.model';
 
 // get all reviews
 const getAllReviewsFromDB = async (
@@ -56,32 +58,80 @@ const getAllListingReviewsFromDB = async (listingId: string) => {
 
 // post a review
 const createReviewIntoDB = async (payload: Partial<TReview>) => {
-  const isReviewExists = await ReviewModel.findOne({
-    userId: payload.userId,
-    listingId: payload.listingId,
-  });
+  const session = await mongoose.startSession();
+  session.startTransaction();
 
-  if (isReviewExists) {
-    throw new AppError(
-      httpStatus.BAD_REQUEST,
-      'You have already reviewed this product, please update your review',
+  try {
+    const listingExists = await ListingModel.findOne({
+      listingId: payload.listingId,
+    });
+
+    if (!listingExists) {
+      throw new AppError(httpStatus.NOT_FOUND, 'This listing does not exist');
+    }
+
+    const isReviewExists = await ReviewModel.findOne({
+      userId: payload.userId,
+      listingId: payload.listingId,
+    });
+
+    if (isReviewExists) {
+      throw new AppError(
+        httpStatus.BAD_REQUEST,
+        'You have already reviewed this product, please update your review',
+      );
+    }
+
+    const isUserOrdered = await OrderModel.findOne({
+      buyerId: payload.userId,
+      listingId: payload.listingId,
+    });
+
+    if (!isUserOrdered) {
+      throw new AppError(
+        httpStatus.NOT_FOUND,
+        'You are not allowed to review a product that you did not order',
+      );
+    }
+
+    const result = await ReviewModel.create([payload], { session });
+
+    if (!result) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create review');
+    }
+
+    const newRating =
+      (listingExists.rating!.rating! + payload.rating!) /
+        listingExists.rating!.totalRating! +
+      1;
+
+    const updatedListing = await ListingModel.findOneAndUpdate(
+      { listingId: listingExists.listingId },
+      {
+        rating: {
+          rating: newRating,
+          totalRating: listingExists.rating!.totalRating! + 1,
+        },
+      },
+      { new: true, session },
     );
+
+    if (!updatedListing) {
+      throw new AppError(
+        httpStatus.BAD_REQUEST,
+        'Failed to update listing rating',
+      );
+    }
+
+    await session.commitTransaction();
+    await session.endSession();
+
+    return result[0];
+  } catch (error) {
+    await session.abortTransaction();
+    await session.endSession();
+    throw error;
   }
-
-  const isUserOrdered = await OrderModel.findOne({
-    buyerId: payload.userId,
-    listingId: payload.listingId,
-  });
-
-  if (!isUserOrdered) {
-    throw new AppError(
-      httpStatus.NOT_FOUND,
-      'You are not allowed to review a product that you did not order',
-    );
-  }
-
-  const result = await ReviewModel.create(payload);
-  return result;
 };
 
 // update a review
